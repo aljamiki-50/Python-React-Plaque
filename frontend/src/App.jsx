b1801d6821716af75718ff1dba0465c725a45fe7
aljamiki-50 <almazin77@yahoo.com>
2024-08-07 22:38:27 +0100

    adding base url there

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import viteLogo from "/vite.svg";
 import { Button, Container, Stack, Text } from "@chakra-ui/react";
 import Navbar from "./components/navbar/Navbar";
 import UserGrid from "./components/UserGrid/UserGrid";
 
-export const BaseUrl = "http://127.0.0.1:5000/api";
+export const BaseUrl =
+  "https://python-react-plaque.onrender.com/api" ||
+  "http://127.0.0.1:5000/api";
 
 function App() {
   const [users, setUsers] = useState([]);
